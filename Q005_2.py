3de428d02f80fed44dbdfbc408f89988a96a0bf2
Tai Koeda <t.koeda@cscloud.co.jp>
2025-05-13 13:41:10 +0900

    Q005_2

---
+++ Q005_2.py
@@ -0,0 +1,29 @@
+h, w = map(int, input().split())
+# h = rows
+# w = columns
+
+gridA = []
+gridB = []
+
+for i in range(h):
+    gridA.append(list(input()))
+
+for i in range(h):
+    gridB.append(list(input()))
+
+
+def shift(grid, rows, cols):
+    return [
+        [grid[(rows + j) % h][(cols + i) % w] for i in range(w)] for j in range(h)
+    ]
+
+
+match = False
+for i in range(h):
+    for j in range(w):
+        if shift(gridA, i, j) == gridB:
+            match = True
+            break
+
+
+print("Yes" if match else "No")
